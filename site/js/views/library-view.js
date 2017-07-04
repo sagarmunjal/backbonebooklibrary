463a4917f5016ad3d6619f2eb56c57ba3ad9c647
sagarmunjal <sagarmunjal@gmail.com>
2017-07-04 14:57:07 +0530

    moving the code from app.js to library.js

--- site/js/views/library-view.js
+++ site/js/views/library-view.js
@@ -1,11 +1,18 @@
 var app = app || {};
 
 app.LibraryView = Backbone.View.extend({
-  initialize : function(){
+  initialize : function(initialCollection){
+    // writing bare javscript related to templating below
+
+    var template = _.template($("#booktemplate").html());
+    $('#target').html(template({initialCollection}))
+
+
     console.log('libraryView initialized');
     this.render();
   },
   render: function(){
+
     console.log('libraryview rendering')
   }
 })
