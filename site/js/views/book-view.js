12a7ad809f13747673fe707d2598360b8c117295
sagarmunjal <sagarmunjal@gmail.com>
2017-07-05 15:32:01 +0530

    code moved further into bookView and logged test messages

--- site/js/views/book-view.js
+++ site/js/views/book-view.js
@@ -1,12 +1,16 @@
 var app = app || {};
 
 app.BookView = Backbone.View.extend({
+  tagname:'div',
+  class: 'target',
+  template : _.template($("#booktemplate").html()),
   initialize : function(){
     console.log('bookView initialized');
-    this.render();
   },
   render: function(){
-    console.log("bookView rendering started")
+    this.$el.html(this.template(this.model.attributes));
+    console.log("bookView rendering started");
+    return this;
   }
 
 })
--- site/js/views/library-view.js
+++ site/js/views/library-view.js
@@ -1,20 +1,23 @@
 var app = app || {};
 
 app.LibraryView = Backbone.View.extend({
-  el : '#target',
-  template : _.template($("#booktemplate").html()),
+  el : '#books',
   initialize : function(initialCollection){
     console.log('libraryView initialized');
-    this.render();
+    this.collection = new app.LibraryCollection(initialCollection);
+    this.render(initialCollection);
   },
   render: function(i){
-    this.$el.html(this.template({i}));
-    console.log('libraryview rendering')
-    this.renderBook();
-  },
-  renderBook: function(){
 
-    new app.BookView({});
+    console.log('libraryview rendering')
+    this.renderBook(i);
+  },
+  renderBook: function(i){
+    console.log('renderBook invoked')
+    var bookView = new app.BookView({
+      model : i
+    });
+    this.$el.append(bookView.render().el)
 
   }
 })
