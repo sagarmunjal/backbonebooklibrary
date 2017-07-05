0b9a5e5d7ebb2201969d3089994132343048086d
sagarmunjal <sagarmunjal@gmail.com>
2017-07-05 16:44:54 +0530

    form data  fetching and listenTo backbone function added in libraryView.js to fetch userinput data

--- site/js/views/library-view.js
+++ site/js/views/library-view.js
@@ -2,15 +2,30 @@ var app = app || {};
 
 app.LibraryView = Backbone.View.extend({
   el : '#books',
+  events : {'click #add' : 'addBook'},
+  addBook : function(e){
+    e.preventDefault()
+    var formData = {};
+    $('#addBook div').children('input').each(
+      function(i,el){
+        if($(el).val() !== ''){
+          formData[el.id] = $(el).val();
+        }
+      });
+      this.collection.add(new app.BookModel(formData));
+  },
   initialize : function(initialCollection){
     console.log('libraryView initialized');
     this.collection = new app.LibraryCollection(initialCollection);
     this.render(initialCollection);
+    this.listenTo(this.collection,'add',this.renderBook)
   },
   render: function(i){
-
+    this.collection.each(function(i){
+        this.renderBook(i);
+    },this)
     console.log('libraryview rendering')
-    this.renderBook(i);
+
   },
   renderBook: function(i){
     console.log('renderBook invoked')
