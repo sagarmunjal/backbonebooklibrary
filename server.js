4bd3a9f45d1365b156332ec9d084ec8cda353799
sagarmunjal <sagarmunjal@gmail.com>
2017-07-13 01:34:58 +0530

    HTTP verbs added in server.js

--- server.js
+++ server.js
@@ -4,14 +4,53 @@ var app_root = __dirname,
   path = require('path'),
   mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/library_database");
 
+//schema
+var myBookSchema = new mongoose.Schema({
+  title : String,
+  author : String,
+  releaseDate : Date
+});
+
+
+//models
+var BookModelDB = mongoose.model('BookModel',myBookSchema);
 
 var app = express();
-
 app.use(express.static(path.join(app_root,'site')));
 app.use(express.static("node_modules"));
 app.use(bodyparser.urlencoded({extended : true}));
 app.use(bodyparser.json());
+
+// get all books
+app.get('/api/books',function(request,response){
+  return BookModelDB.find(function(err,books){
+    if(!err){
+      return response.send(books);
+    }else{
+      return console.log(err);
+    }
+  })
+})
+
+// add a new book
+
+app.post('/api/books',function(request,response){
+  var book = new BookModelDB({
+    title:request.body.title,
+    author:request.body.author,
+    releaseDate:request.body.releaseDate
+  });
+  return book.save(function(err){
+    if(!err){
+      console.log('new book added');
+      return response.send(book);
+    }else{
+      console.log(err);
+    }
+  })
+})
 
 var port = 4711;
 
